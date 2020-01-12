3ebc1476e30293f36465e9c7e784bab2e6040fa0
thejaigoyal <thejaigoyal@gmail.com>
2020-01-13 00:15:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class Custom extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      new: "newß",
+      otherproperty: ""
+    };
+  }
+  componentDidMount() {
+    this.setState({ otherproperty: "revised" });
+  }
+  render() {
+    console.log(this.state);
+    return <div className="App">comming from custom component</div>;
+  }
+}
+
+// JSX - Javascript XML
+// immutable
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Custom abc="abc prop"></Custom>
       </header>
     </div>
   );
